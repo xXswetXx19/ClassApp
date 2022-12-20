dfde4e63cfa6e4ac5b61278818690cadfe5acb46
Your Name <you@example.com>
2022-12-20 14:29:41 -0500

    Avanzando ando

--- calend.py
+++ calend.py
@@ -1,96 +1,87 @@
 from tkinter import *
+from tkinter import ttk
 import calendar
 
 
 class Dia(Label):
     def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.__Prioridad = 0
-        self.__Descripcion = ["Hacer mates", "Dominar el mundo", "perriar hasta el piso"]
-        
-    def setPrioridad(self, prioridad):
-        self.__Prioridad = prioridad
-    def setDescripcion(self, descripcion):
-        self.__Descripcion.append(descripcion)
-    def getPrioridad(self):
-        return self.__Prioridad
-    def getDescripcion(self):
-        return self.__Descripcion
+        date = kwargs.pop('date', None)
+        super().__init__(*args,**kwargs)
 
+        self.Dia = date.day
+        self.Mes = date.month
+        self.Año = date.year
+
+        self.__Notas = [
+            ["Urgente", "Ir al Tuti"],
+            ["Baja", "Hacer mates"],
+            ["Baja", "Hacer Dominar el sss"],
+            ["Media", "Dominar el mundo"],
+            ["Alta", "Perriar hasta el piso"],
+            ["Baja", "Wacamole"],
+            ["Alta", "Hacer la tarea"],
+            ["Urgente", "Ir al doctor"]
+        ]
+    def addNota(self, descripcion):
+        self.__Notas.append(descripcion)
+    def getDescripcion(self):
+        self.__Notas.sort(key=lambda x: ["Baja", "Media", "Alta","Urgente"].index(x[0]), reverse=True)
+        return self.__Notas
+    def getday(self):
+        return self.Dia
+    def getFecha(self):
+        return self.Fecha
 
 class Ventanita:
-    def __init__(self, label):
+    def __init__(self, widgetday):
         win = Toplevel()
-        win.title("Prioridad")
+        win.title("Tareas")
         win.geometry("400x200")
-        win.resizable(0,0)
+        # win.resizable(0,0)
         color1 = "#eeeeee"
         color2 = "#F5F5DC"
         self.TopFrame = Frame(win, bg=color1)
         self.ButtonsFrame = Frame(win, bg="red")
-        self.HomeworksFrame = Frame(self.TopFrame, bg=color2, bd=4, relief="ridge", width=400, height=150)
-        # groove, sunken, flat 
-        # ridge
+        self.HomeworksFrame = Frame(self.TopFrame, bg=color1, bd=4, relief="ridge", width=400, height=150)
+    
         self.TopFrame.pack(expand= True, fill=BOTH, side=TOP)
         self.HomeworksFrame.grid(row=1, column=0, sticky='nsew')
         self.ButtonsFrame.pack(fill=BOTH, side=BOTTOM)
 
-        self.TopFrame.grid_rowconfigure(1, minsize=130)
+        self.TopFrame.grid_rowconfigure(1, minsize=150)
         self.TopFrame.grid_columnconfigure(0, minsize=400)
 
-
-        LTareas = Label(self.TopFrame, text="Tareas", font=("Arial", 11, "bold"), background=color1)
+        LTareas = Label(self.TopFrame, text=widgetday.getday(), font=("Arial", 11, "bold"), background=color1)
         LTareas.grid(row=0, column=0, sticky=W)
+        
+        self.view_tree = ttk.Treeview(self.HomeworksFrame, height= 6,columns = 2)
+        self.view_tree.grid(row = 0, column = 0)
+        self.view_tree.heading("#0", text = "Descripción")
+        self.view_tree.heading("#1", text = "Prioridad")
+        self.view_tree.column("#0", width = 320, anchor=W)
+        self.view_tree.column("#1", width = 70, anchor="center")
+        
+        # Creando los tags
+        self.view_tree.tag_configure("Baja", background="#D5F5E3")
+        self.view_tree.tag_configure("Media", background="#F5E6C0")
+        self.view_tree.tag_configure("Alta", background="#E6B0AA")
+        self.view_tree.tag_configure("Urgente", background="#FF5252")
+        
+        # Ponerle bordes a las filas del treeview
+        for Prioridad, Nota in widgetday.getDescripcion():
+            row = self.view_tree.insert("", "end", text=Nota, values=Prioridad)
+            if Prioridad == "Baja":
+                self.view_tree.item(row, tags="Baja")
+            elif Prioridad == "Media":
+                self.view_tree.item(row, tags="Media")
+            elif Prioridad == "Alta":
+                self.view_tree.item(row, tags="Alta")
+            elif Prioridad == "Urgente":
+                self.view_tree.item(row, tags="Urgente")
+            
+        Button(self.ButtonsFrame, text="Añadir Pendiente", font=("Arial", 11, "bold"), background=color1, command=(), width=20).pack(side=LEFT)
+        Button(self.ButtonsFrame, text="Eliminar Pendiente", font=("Arial", 11, "bold"), background=color1, command=(), width=25).pack(side=RIGHT)
 
-        for i in range(len(label.getDescripcion())):
-            Label(self.HomeworksFrame, text=label.getDescripcion()[i], font=("Arial", 11, "bold"), background=color2).grid(row=i+1, column=0, sticky=W)        
-        
-        Button(self.ButtonsFrame, text="Añadir Pendiente", font=("Arial", 11, "bold"), background="white", command=(), width=20).pack(side=LEFT)
-        Button(self.ButtonsFrame, text="Eliminar Pendiente", font=("Arial", 11, "bold"), background="white", command=(), width=25).pack(side=RIGHT)
-    #     # create a label for the priority
-    #     Label(top, text="Prioridad", font=("Arial", 11, "bold"), background="white").pack()
-    #     Label(top, text=label.getPrioridad(), font=("Arial", 11, "bold"), background="white").pack()
-    #     # create a label for the description
-    #     Label(top, text="Descripción", font=("Arial", 11, "bold"), background="white").pack()
-    #     for i in label.getDescripcion():
-    #         Label(top, text=i, font=("Arial", 11, "bold"), background="white").pack()
-    #     # create a button for add a new description
-    #     Button(top, text="Añadir descripción", font=("Arial", 11, "bold"), background="white", command=lambda: self.addDescripcion(label)).pack()
-    #     # create a button for add a new priority
-    #     Button(top, text="Añadir prioridad", font=("Arial", 11, "bold"), background="white", command=lambda: self.addPrioridad(label)).pack()
-    #     # create a button for delete the priority
-    #     Button(top, text="Eliminar prioridad", font=("Arial", 11, "bold"), background="white", command=lambda: self.deletePrioridad(label)).pack()
-    #     # create a button for delete the description
-    #     Button(top, text="Eliminar descripción", font=("Arial", 11, "bold"), background="white", command=lambda: self.deleteDescripcion(label)).pack()
-    # def addDescripcion(self, label):
-    #     top = Toplevel()
-    #     top.title("Añadir descripción")
-    #     top.geometry("300x300")
-    #     top.resizable(0,0)
-    #     top.config(cursor="hand2")
-    #     top.config(bd=15)
-    #     top.config(relief="groove")
-        
-    #     Label(top, text="Descripción", font=("Arial", 11, "bold"), background="white").pack()
-    #     Entry(top, textvariable=StringVar(), font=("Arial", 11, "bold"), background="white").pack()
-    #     Button(top, text="Añadir", font=("Arial", 11, "bold"), background="white", command=lambda: self.addDescripcion2(label)).pack()
-    # def addDescripcion2(self, label):
-    #     label.addDescripcion("Hola")
-    #     print(label.getDescripcion())
-    # def addPrioridad(self, label):
-    #     top = Toplevel()
-    #     top.title("Añadir prioridad")
-    #     top.geometry("300x300")
-    #     top.resizable(0,0)
-    #     top.config(cursor="hand2")
-        
-    #     Label(top, text="Prioridad", font=("Arial", 11, "bold"), background="white").pack()
-    #     Entry(top, textvariable=StringVar(), font=("Arial", 11, "bold"), background="white").pack()
-    #     Button(top, text="Añadir", font=("Arial", 11, "bold"), background="white", command=lambda: self.addPrioridad2(label)).pack()
-    # def addPrioridad2(self, label):
-    #     label.addPrioridad("Hola")
-    #     print(label.getPrioridad())
-    
 
 class Calendario:
     def __init__ (self, window):
@@ -100,7 +91,7 @@ class Calendario:
         self.window.title("Calendario")
         self.window.resizable(0,0)
         self.TopFrame = Frame(self.window,background="#006064", height=50)
-        self.CenterFrame = Frame(self.window, background="green", height=30)
+        self.CenterFrame = Frame(self.window, background="black", height=30)
         self.BotFrame = Frame(self.window,background="red", height=205)
         # Frames
         self.TopFrame.pack(fill = BOTH, side=TOP)
@@ -132,7 +123,7 @@ class Calendario:
         for i, week in enumerate(cal):
             for j, day in enumerate(week):
                 if day.month == 12: # Si el día pertenece al mes de enero de 2022, crea un botón
-                    label = Dia(self.BotFrame, text=day.day, bg="#eeeeee", font="Arial 11", width=10, height=2, border=8)
+                    label = Dia(self.BotFrame, text=day.day, bg="#eeeeee", font="Arial 11", width=10, height=2, border=8, date=day)
                     label.grid(row=i, column=j)
                     label.bind("<Button-1>", self.on_click_1)
                     label.bind("<Button-3>", self.on_click_2)
@@ -140,7 +131,7 @@ class Calendario:
                     label.bind("<Leave>", self.onleave)
 
                 else:
-                    label = Dia(self.BotFrame, text=day.day, bg="#D3D3D3", fg="#808080", font="Arial 11", width=10, height=2, border=8)
+                    label = Dia(self.BotFrame, text=day.day, bg="#D3D3D3", fg="#808080", font="Arial 11", width=10, height=2, border=8, date=day)
                     label.grid(row=i, column=j)
         
         self.selected_label = []
