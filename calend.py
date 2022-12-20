e2065e6eb2ffd3b5cc6dc33b8af36e508ccd390d
SamuelPcCasa <samuelsebastiangonzalez@gmail.com>
2022-12-19 23:21:25 -0500

    Avanzando en la ventana pequeña

--- calend.py
+++ calend.py
@@ -24,19 +24,29 @@ class Ventanita:
         win.title("Prioridad")
         win.geometry("400x200")
         win.resizable(0,0)
-        self.TopFrame = Frame(win, bg="blue")
+        color1 = "#eeeeee"
+        color2 = "#F5F5DC"
+        self.TopFrame = Frame(win, bg=color1)
         self.ButtonsFrame = Frame(win, bg="red")
-        self.HomeworksFrame = Frame(self.TopFrame, bg="green", bd=4, relief="groove", padx=120, pady=20)
-        
+        self.HomeworksFrame = Frame(self.TopFrame, bg=color2, bd=4, relief="ridge", width=400, height=150)
+        # groove, sunken, flat 
+        # ridge
         self.TopFrame.pack(expand= True, fill=BOTH, side=TOP)
-        self.HomeworksFrame.grid(row=1, column=0, sticky=W)
-        self.ButtonsFrame.pack(expand= True, fill=BOTH, side=BOTTOM)
+        self.HomeworksFrame.grid(row=1, column=0, sticky='nsew')
+        self.ButtonsFrame.pack(fill=BOTH, side=BOTTOM)
 
-        LTareas = Label(self.TopFrame, text="Tareas", font=("Arial", 11, "bold"), background="white")
+        self.TopFrame.grid_rowconfigure(1, minsize=130)
+        self.TopFrame.grid_columnconfigure(0, minsize=400)
+
+
+        LTareas = Label(self.TopFrame, text="Tareas", font=("Arial", 11, "bold"), background=color1)
         LTareas.grid(row=0, column=0, sticky=W)
+
         for i in range(len(label.getDescripcion())):
-            Label(self.HomeworksFrame, text=label.getDescripcion()[i], font=("Arial", 11, "bold"), background="white").grid(row=i+1, column=0, sticky=W)        
+            Label(self.HomeworksFrame, text=label.getDescripcion()[i], font=("Arial", 11, "bold"), background=color2).grid(row=i+1, column=0, sticky=W)        
         
+        Button(self.ButtonsFrame, text="Añadir Pendiente", font=("Arial", 11, "bold"), background="white", command=(), width=20).pack(side=LEFT)
+        Button(self.ButtonsFrame, text="Eliminar Pendiente", font=("Arial", 11, "bold"), background="white", command=(), width=25).pack(side=RIGHT)
     #     # create a label for the priority
     #     Label(top, text="Prioridad", font=("Arial", 11, "bold"), background="white").pack()
     #     Label(top, text=label.getPrioridad(), font=("Arial", 11, "bold"), background="white").pack()
@@ -86,7 +96,7 @@ class Calendario:
     def __init__ (self, window):
 # Crea la ventana principal y el calendario
         self.window = window
-        self.window.geometry("755x372")
+        self.window.geometry("755x375")
         self.window.title("Calendario")
         self.window.resizable(0,0)
         self.TopFrame = Frame(self.window,background="#006064", height=50)
@@ -102,9 +112,16 @@ class Calendario:
         cal = c.monthdatescalendar(2022, 12)
 
         # Top Frame
-        self.butt1 = Label(self.TopFrame, text = "<", width= 5, height=3, font=(10), background= "#006064").pack(side = LEFT, anchor=CENTER)
-        self.butt2 = Label(self.TopFrame, text = ">", width= 5, height=3, font=(10), background= "#006064").pack(side = RIGHT, anchor=CENTER)
+        self.butt1 = Label(self.TopFrame, text = "<",width= 5, height=3, font=(10), background= "#006064", foreground= "white")
+        self.butt1.pack(side = LEFT, anchor=CENTER)
+        self.butt2 = Label(self.TopFrame, text = ">", width= 5, height=3, font=(10), background= "#006064", foreground= "white")
+        self.butt2.pack(side = RIGHT, anchor=CENTER)
         self.MesLabel = Label(self.TopFrame, text= "Diciembre",background="#006064",foreground= "#FFFFFF", font=("Arial Black", 21)).pack(anchor=CENTER, expand=True)
+
+        self.butt1.bind("<Enter>", lambda event: self.butt1.config(background="#004d4d"))
+        self.butt1.bind("<Leave>", lambda event: self.butt1.config(background="#006064"))
+        self.butt2.bind("<Enter>", lambda event: self.butt2.config(background="#004d4d"))
+        self.butt2.bind("<Leave>", lambda event: self.butt2.config(background="#006064"))
 
         # Center Frame
         Dias = ("Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo")
@@ -119,6 +136,9 @@ class Calendario:
                     label.grid(row=i, column=j)
                     label.bind("<Button-1>", self.on_click_1)
                     label.bind("<Button-3>", self.on_click_2)
+                    label.bind("<Enter>", self.onover)
+                    label.bind("<Leave>", self.onleave)
+
                 else:
                     label = Dia(self.BotFrame, text=day.day, bg="#D3D3D3", fg="#808080", font="Arial 11", width=10, height=2, border=8)
                     label.grid(row=i, column=j)
@@ -126,7 +146,7 @@ class Calendario:
         self.selected_label = []
     def on_click_1(self, event):
         label = event.widget
-        if label["background"] == "#c3c3c3":
+        if label["background"] == "#c3c3c3" and label in self.selected_label:
             label.config(background="#eeeeee")
         else:
             for i in self.selected_label:
@@ -137,6 +157,13 @@ class Calendario:
     def on_click_2(self,event):
         label = event.widget
         Ventanita(label)
+    def onover(self, event):
+        label = event.widget
+        label.config(background="#c3c3c3")
+    def onleave(self, event):
+        label = event.widget
+        if label not in self.selected_label:
+            label.config(background="#eeeeee")
 
 root = Tk()
 Calendario(root)
